801d37ee2da528c3f3cdbef3ed491cf69326ff60
Ivan Yurin <yurin.ivan@gmail.com>
2014-03-12 21:17:30 +0600

    поправлена структура

--- fabric_contrib/django.py
+++ fabric_contrib/django.py
@@ -6,6 +6,13 @@ COMMAND_COLLECTSTATIC = 'collectstatic'
 COMMAND_SYNCDB = 'syncdb'
 COMMAND_MIGRATE = 'migrate'
 
+_default_command = '{python} {manage} {command}'
+_commands_list = {
+    COMMAND_COLLECTSTATIC: 'yes yes | {python} {manage} {command}',
+    COMMAND_MIGRATE: '{python} {manage} {command} --noinput -v0 --merge '
+                     '--ignore-ghost-migrations',
+}
+
 
 def django_commands():
     for command in env.django_commands:
@@ -13,18 +20,11 @@ def django_commands():
 
 
 def _django_command(command):
-    _command = {
-        'default': '{python} {manage} {command}',
-
-        COMMAND_COLLECTSTATIC: 'yes yes | {python} {manage} {command}',
-        COMMAND_MIGRATE: '{python} {manage} {command} --noinput -v0 --merge '
-                         '--ignore-ghost-migrations',
-    }
-
-    command = _command.get(command, command['default'])
-    command.format(
+    command_to_run = _commands_list.get(command, _default_command)
+    command_to_run = command_to_run.format(
         python=env.server_python,
         manage=env.server_manage,
         command=command
     )
-    run(command)
+
+    run(command_to_run)
