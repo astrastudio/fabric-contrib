6e5e365a196f89e4a3debaed87552f75dfc3076c
Ivan Yurin <yurin.ivan@gmail.com>
2014-03-12 20:00:20 +0600

    add django_command

--- fabric_contrib/django.py
+++ fabric_contrib/django.py
@@ -1,16 +1,25 @@
 # coding=utf-8
-from fabric.api import run
+from fabric.api import env, run
 
 
-def django_collectstatic(python, manage):
-    run('yes yes | {} {} collectstatic'.format(python, manage))
+COMMAND_COLLECTSTATIC = 'collectstatic'
+COMMAND_SYNCDB = 'syncdb'
+COMMAND_MIGRATE = 'migrate'
 
 
-def django_syncdb(python, manage):
-    run('{} {} syncdb'.format(python, manage))
+def django_command(command):
+    _command = {
+        'default': '{python} {manage} {command}',
 
+        COMMAND_COLLECTSTATIC: 'yes yes | {python} {manage} {command}',
+        COMMAND_MIGRATE: '{python} {manage} {command} --noinput -v0 --merge '
+                         '--ignore-ghost-migrations',
+    }
 
-def django_migrate(python, manage):
-    cmd = '{} {} migrate --noinput -v0 --merge --ignore-ghost-migrations'\
-        .format(python, manage)
-    run(cmd)
+    command = _command.get(command, command['default'])
+    command.format(
+        python=env.server_python,
+        manage=env.server_manage,
+        command=command
+    )
+    run(command)
