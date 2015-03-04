576ec143d472a1e1762a44833dd53695b6ad6153
rremizov <rremizov@yandex.ru>
2015-03-04 19:57:26 +0500

    возможность определять параметры окружения при вызове django commands

--- fabric_contrib/django.py
+++ fabric_contrib/django.py
@@ -14,12 +14,12 @@ _commands_list = {
 }
 
 
-def django_commands():
+def django_commands(os_environment=None):
     for command in env.django_commands:
-        _django_command(command)
+        _django_command(command, os_environment)
 
 
-def _django_command(command):
+def _django_command(command, os_environment):
     command_to_run = _commands_list.get(command, _default_command)
     command_to_run = command_to_run.format(
         python=env.server_python,
@@ -27,4 +27,14 @@ def _django_command(command):
         command=command
     )
 
-    run(command_to_run)
+    if os_environment is None:
+        run(command_to_run)
+        return
+
+    prefix = ' '.join([
+        '{}={}'.format(k, v)
+        for k, v in os_environment.items()
+    ])
+
+    run('{} {}'.format(prefix, command_to_run))
+
